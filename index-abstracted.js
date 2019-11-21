75705d221773f03dd181ca7271044624982e6c2f
Shirley Wong <shirleywsywong@gmail.com>
2019-11-21 14:40:58 -0500

    first commit

---
+++ index-abstracted.js
@@ -0,0 +1,83 @@
+const search = {
+    getContent: function() {
+        return document.getElementsByTagName('article')[0].innerHTML
+    },
+    getUserInput: function() {
+        document.getElementById("getSearchInput").onclick = function (event) {
+            event.preventDefault();
+            document.getElementById("error").innerHTML = ("")
+            let inputText = document.getElementById("searchField");
+            console.log(inputText.value)
+            return inputText.value;
+        }
+    },
+    checkUserInput: function() {
+        if (this.getUserInput === '') {
+            console.log('checkInput')
+            document.getElementById("error").innerHTML = (`Please enter a valid search term`)
+        }
+    },
+    findMatch: function(input) {
+        let match = [...content.matchAll(input)]
+    },
+    checkMatch: function(matches) {
+        if (matches.length == 0) {
+            return document.getElementById("error").innerHTML = (`Your search did not return any result.`)
+        }
+    },
+    recordMatch: function(matches) {
+        let word = matches[0][0];
+        let wordStart = [];
+        let wordEnd = [];
+
+        for (const match of matches) {
+            wordStart.push(match.index)
+            wordEnd.push(match.index + match[0].length)
+        }
+
+        highlightMatch(word, wordStart, wordEnd)
+    }
+
+}
+
+//get all the content in the article
+
+
+//get the search term from the input field
+
+
+//find the matching word in the content
+
+//check to see if there's any search result
+
+//record where the word is found
+
+
+//highlight the matched word in the text
+function highlightMatch(word, wordStart, wordEnd) {
+
+    //replace the matched word with highlighting span
+    let highlight = word.replace(word, `<span class="match">` + word + `</span>`)
+
+    //get the content from the beginning to where the first word is
+    let preText = content.substring(0, wordStart[0])
+
+    //get all the content from where the 1st word ends to where the 2nd word begin
+    let midText = []
+    for (let i = 0; i < wordStart.length; i++) {
+        midText.push(content.substring(wordEnd[i], wordStart[i + 1]))
+    }
+
+    //assemble the highlighted word with the midtext
+    let midCount = 0;
+    let postText = [];
+    while (midCount < wordStart.length) {
+        postText.push(highlight + midText[midCount])
+        midCount++
+    }
+
+    //push everything to the DOM
+    document.getElementsByTagName('article')[0].innerHTML =
+        (preText + postText.join(" "))
+
+}
